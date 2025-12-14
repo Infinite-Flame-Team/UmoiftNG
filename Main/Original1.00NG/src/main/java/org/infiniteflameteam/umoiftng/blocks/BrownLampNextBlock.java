177752a3729a8e9353312fee5290c8249fe6a3f3
Hei_wan_Feng <Hei_wan_Feng@Offical.hwf>
2025-12-14 19:45:11 +0800

    Main-Original-Version-1.00-NG-UmoiftVersion-0.0.0.5

---
+++ Main/Original1.00NG/src/main/java/org/infiniteflameteam/umoiftng/blocks/BrownLampNextBlock.java
@@ -0,0 +1,14 @@
+package org.infiniteflameteam.umoiftng.blocks;
+
+import net.minecraft.world.level.block.state.BlockBehaviour;
+
+public class BrownLampNextBlock extends AbstractColoredLampNextBlock {
+    public BrownLampNextBlock(BlockBehaviour.Properties properties) {
+        super(properties);
+    }
+
+    @Override
+    public String getColorName() {
+        return "brown";
+    }
+}
