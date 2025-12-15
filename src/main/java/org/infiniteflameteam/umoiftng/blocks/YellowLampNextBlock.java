a58dc45ae4ed3129756ab71fdc9112cc46cda8a8
Hei_wan_Feng <Hei_wan_Feng@Offical.hwf>
2025-12-15 20:25:35 +0800

    Main-Original-Version-1.00-NG-UmoiftVersion-0.0.0.5

---
+++ src/main/java/org/infiniteflameteam/umoiftng/blocks/YellowLampNextBlock.java
@@ -0,0 +1,14 @@
+package org.infiniteflameteam.umoiftng.blocks;
+
+import net.minecraft.world.level.block.state.BlockBehaviour;
+
+public class YellowLampNextBlock extends AbstractColoredLampNextBlock {
+    public YellowLampNextBlock(BlockBehaviour.Properties properties) {
+        super(properties);
+    }
+
+    @Override
+    public String getColorName() {
+        return "yellow";
+    }
+}
